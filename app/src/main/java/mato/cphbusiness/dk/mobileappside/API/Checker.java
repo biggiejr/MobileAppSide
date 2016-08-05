0b347b88c97b6ae6cb3e0302bc495e9f03ee9b1e
biggiejr <jrmacej@gmail.com>
2016-08-05 15:48:20 +0200

    oldFileList operations
    updating, initializing in shared preferences

--- app/src/main/java/mato/cphbusiness/dk/mobileappside/API/Checker.java
+++ app/src/main/java/mato/cphbusiness/dk/mobileappside/API/Checker.java
@@ -1,5 +1,9 @@
 package mato.cphbusiness.dk.mobileappside.API;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -7,28 +11,56 @@ import java.util.List;
 /**
  * Created by Mato on 05.08.16.
  */
-public class Checker {
+public abstract class Checker extends Context {
 
+    SharedPreferences sp;
+    static ArrayList old = new ArrayList();
 
-    private void compareLists(ArrayList<String> current, ArrayList<String> old){
+    public static void loadArray(Context mContext){
+
+        SharedPreferences mSharedPreference1 =   PreferenceManager.getDefaultSharedPreferences(mContext);
+        old.clear();
+        int size = mSharedPreference1.getInt("Status_size", 0);
+
+        for(int i=0;i<size;i++)
+        {
+            old.add(mSharedPreference1.getString("Status_" + i, null));
+        }
+
+    }
+
+    private void compareLists(ArrayList<String> current){
 
         Collection<String> original = new ArrayList(old);
         Collection<String> updated = new ArrayList(current);
 
-
         List<String> toDelete = new ArrayList<String>(original);
         List<String> toDownload = new ArrayList<String>(updated);
-
 
         toDelete.removeAll(updated);
         toDownload.removeAll(original);
 
-
         System.out.println( toDelete );                                 //what should be deleted
         System.out.println( toDownload );                               //what should be downloaded
 
+        saveNewList((ArrayList<String>) updated);
 
+    }
 
+    private boolean saveNewList(ArrayList<String> temp){
+
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putInt("Status_size", temp.size());
+
+        for(int i=0;i<temp.size();i++)
+        {
+            editor.remove("Status_" + i);
+            editor.putString("Status_" + i, temp.get(i));
+        }
+
+        return editor.commit();
     }
 
 }
